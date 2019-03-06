b8f036c6f662d0e07cf0f7df8e2ef3fc0612ddb2
Negite <47744442+Negite@users.noreply.github.com>
2019-03-06 13:50:01 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<? header('Content-Type: text/html; charset=utf-8'); ?>
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+<body>
+<div style="width: 1000px; padding: 50px; margin: auto;">
+    <h2>Эта форма отправляет методом GET</h2>
+    <form method="get" action="get.php">
+        <input type="text" name="name" /><br />
+        <input type="text" name="message" /><br />
+        <input type="submit" />        
+    </form>
+    <h2>Эта форма отправляет методом POST</h2>
+    <form method="post" action="post.php">
+        <input type="text" name="name" placeholder="Заголовок письма"/><br />
+        <textarea rows="10" cols="45" name="message"  placeholder="Заголовок письма"></textarea><br />
+
+        <p>Radio</p>
+        <input type="radio" name="browser" value="IE"> Internet Explorer<br />
+        <input type="radio" name="browser" value="Opera"> Opera<br />
+        <input type="radio" name="browser" value="Firefox"> Firefox<br />
+
+        <p>Checkbox</p>
+        <input type="checkbox" name="tvchannel" value="Первый канал" />Первый канал<br />
+        <input type="checkbox" name="tvchannel" value="THT" />ТНТ<br />
+        <input type="checkbox" name="tvchannel" value="Культура" />Культура<br />
+
+        <input type="hidden" name="name_hidden" value="Negite">
+    
+        <input type="submit" />        
+    </form>
+    </div>
+</body>
+</html>
---
+++ post.php
@@ -0,0 +1,23 @@
+﻿<? 
+
+$name = $_POST['name'];
+echo 'Тема Письма: '.$name.'<br/>';
+
+
+$message = $_POST['message'];
+echo 'Сообщение в письме: '.$message.'<br/>'; 
+
+$browser = $_POST['browser'];
+echo 'Браузер: '.$browser.'<br />';
+    
+
+$tvchannel = $_POST['tvchannel'];
+echo 'Каналы: '.$tvchannel.'<br />';
+
+$name_hidden = $_POST['name_hidden'];
+echo 'Секретное поле: '.$name_hidden.'<br />';
+
+//mail("negite@mail.ru", "$name", "$message", "From: negite@mail.ru");
+
+
+?>
